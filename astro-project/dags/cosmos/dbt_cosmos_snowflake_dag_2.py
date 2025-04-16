9695423296f845dd87b177c130a43c5c22b83b6e
caitlin-petro <caitlin.petro@astronomer.io>
2025-04-16 14:32:23 -0400

    add second cosmos dag

---
+++ astro-project/dags/cosmos/dbt_cosmos_snowflake_dag_2.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import os
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
+from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+
+
+profile_config = ProfileConfig(
+    profile_name="my_profile_name",
+    target_name="my_target_name",
+    profile_mapping=SnowflakeUserPasswordProfileMapping(
+        conn_id="snowflake_dev",
+    ),
+)
+
+airflow_home = os.environ['AIRFLOW_HOME']
+
+dbt_snowflake_dag = DbtDag(
+    project_config=ProjectConfig(
+        f"{airflow_home}/dbt/dbtproject",
+    ),
+    operator_args={"install_deps": True},
+    profile_config=profile_config,
+    execution_config=ExecutionConfig(
+        dbt_executable_path=f"{airflow_home}/dbt_venv/bin/dbt",
+    ),
+    render_config=RenderConfig(select=["path:models/analysis", "path:models/transform", "path:models/downstream"]),
+    schedule="@hourly",
+    start_date=datetime(2023, 9, 10),
+    catchup=False,
+    dag_id="dbt_cosmos_snowflake_dag_2",
+    tags=["cosmos"],
+)
